0f721b3ddacf1466f87a7ca91f6cfa4b839e7eaa
VitaliyP12 <v.perchuk04@gmail.com>
2024-06-28 15:23:02 +0300

    Basic cross draw.

--- ui/window.go
+++ ui/window.go
@@ -16,6 +16,59 @@ import (
 	"golang.org/x/mobile/event/size"
 )
 
+var yellowColor = color.RGBA{R: 0xff, G: 0xff, B: 0xf, A: 0xff}
+
+type MyFigure struct {
+	x, y, w, h int
+}
+
+func GetFigure(myX int, myY int) *MyFigure {
+	return &MyFigure{x: myX, y: myY, w: 100, h: 300}
+}
+
+func (f *MyFigure) Draw(t screen.Texture) {
+	x1 := f.x - f.w/2
+	y1 := f.y - f.h/2
+	x2 := f.x + f.w/2
+	y2 := f.y + f.h/2
+	t.Fill(image.Rect(x1, y1, x2, y2), yellowColor, draw.Src)
+
+	x1 = f.x - f.h/2
+	y1 = f.y - f.w/2
+	x2 = f.x + f.h/2
+	y2 = f.y + f.w/2
+	t.Fill(image.Rect(x1, y1, x2, y2), yellowColor, draw.Src)
+}
+
+func (f *MyFigure) VisualizeFigure(pw *Visualizer) {
+	x1 := f.x - f.w/2
+	y1 := f.y - f.h/2
+	x2 := f.x + f.w/2
+	y2 := f.y + f.h/2
+	pw.w.Fill(image.Rect(x1, y1, x2, y2), yellowColor, draw.Src)
+
+	x1 = f.x - f.h/2
+	y1 = f.y - f.w/2
+	x2 = f.x + f.h/2
+	y2 = f.y + f.w/2
+	pw.w.Fill(image.Rect(x1, y1, x2, y2), yellowColor, draw.Src)
+}
+
+func (f *MyFigure) Move(x, y int) {
+	f.x = x
+	f.y = y
+}
+
+func (pw* Visualizer) MoveAllFigures(x, y int) {
+	for _, f := range pw.Figures {
+		f.Move(x, y)
+	}
+}
+
+func (pw* Visualizer) AddFigure(t screen.Texture) {
+	pw.Figures = append(pw.Figures, GetFigure(100, 100))
+}
+
 type Visualizer struct {
 	Title         string
 	Debug         bool
@@ -27,6 +80,7 @@ type Visualizer struct {
 
 	sz  size.Event
 	pos image.Rectangle
+	Figures   []*MyFigure
 }
 
 func (pw *Visualizer) Main() {
@@ -44,6 +98,8 @@ func (pw *Visualizer) Update(t screen.Texture) {
 func (pw *Visualizer) run(s screen.Screen) {
 	w, err := s.NewWindow(&screen.NewWindowOptions{
 		Title: pw.Title,
+		Width: 800,
+		Height: 800,
 	})
 	if err != nil {
 		log.Fatal("Failed to initialize the app window:", err)
@@ -58,6 +114,7 @@ func (pw *Visualizer) run(s screen.Screen) {
 	}
 
 	pw.w = w
+	pw.Figures = []*MyFigure{GetFigure(400, 400)}
 
 	events := make(chan any)
 	go func() {
@@ -114,9 +171,11 @@ func (pw *Visualizer) handleEvent(e any, t screen.Texture) {
 		log.Printf("ERROR: %s", e)
 
 	case mouse.Event:
-		if t == nil {
-			// TODO: Реалізувати реакцію на натискання кнопки миші.
-		}
+		if t != nil { return; }
+		if e.Button != mouse.ButtonRight { return; }
+		if e.Direction != mouse.DirPress { return; }
+		
+		pw.MoveAllFigures(int(e.X), int(e.Y))
 
 	case paint.Event:
 		// Малювання контенту вікна.
@@ -133,7 +192,9 @@ func (pw *Visualizer) handleEvent(e any, t screen.Texture) {
 func (pw *Visualizer) drawDefaultUI() {
 	pw.w.Fill(pw.sz.Bounds(), color.Black, draw.Src) // Фон.
 
-	// TODO: Змінити колір фону та додати відображення фігури у вашому варіанті.
+	for _, f := range pw.Figures {
+		f.VisualizeFigure(pw)
+	}
 
 	// Малювання білої рамки.
 	for _, br := range imageutil.Border(pw.sz.Bounds(), 10) {
